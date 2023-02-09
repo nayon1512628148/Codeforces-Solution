c2dd52ba8c448b18e7e63cb7b5249192069a9607
Sree Nayon Kumar Pal <115805843+nayon1512628148@users.noreply.github.com>
2023-02-09 22:05:41 +0600

    Add files via upload

---
+++ 236 - A. Boy or Girl.cpp
@@ -0,0 +1,153 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i, ln, total=0, a=0, b=0, c=0, d=0, e=0, f=0, g=0, h=0, ii=0, j=0, k=0, l=0, m=0, n=0, o=0, p=0, q=0, r=0, s=0, t=0, u=0, v=0, w=0, x=0, y=0, z=0;
+    string st;
+    cin >> ws;
+    getline(cin, st);
+    ln = st.size();
+    for(i=0; i<ln; i++)
+    {
+        if(st[i]=='a' && a==0)
+        {
+            total = total+1;
+            a++;
+        }
+        else if(st[i]=='b' && b==0)
+        {
+            total = total+1;
+            b++;
+        }
+        else if(st[i]=='c' && c==0)
+        {
+            total = total+1;
+            c++;
+        }
+        else if(st[i]=='d' && d==0)
+        {
+            total = total+1;
+            d++;
+        }
+        else if(st[i]=='e' && e==0)
+        {
+            total = total+1;
+            e++;
+        }
+        else if(st[i]=='f' && f==0)
+        {
+            total = total+1;
+            f++;
+        }
+        else if(st[i]=='g' && g==0)
+        {
+            total = total+1;
+            g++;
+        }
+        else if(st[i]=='h' && h==0)
+        {
+            total = total+1;
+            h++;
+        }
+        else if(st[i]=='i' && ii==0)
+        {
+            total = total+1;
+            ii++;
+        }
+        else if(st[i]=='j' && j==0)
+        {
+            total = total+1;
+            j++;
+        }
+        else if(st[i]=='k' && k==0)
+        {
+            total = total+1;
+            k++;
+        }
+        else if(st[i]=='l' && l==0)
+        {
+            total = total+1;
+            l++;
+        }
+        else if(st[i]=='m' && m==0)
+        {
+            total = total+1;
+            m++;
+        }
+        else if(st[i]=='n' && n==0)
+        {
+            total = total+1;
+            n++;
+        }
+        else if(st[i]=='o' && o==0)
+        {
+            total = total+1;
+            o++;
+        }
+        else if(st[i]=='p' && p==0)
+        {
+            total = total+1;
+            p++;
+        }
+        else if(st[i]=='q' && q==0)
+        {
+            total = total+1;
+            q++;
+        }
+        else if(st[i]=='r' && r==0)
+        {
+            total = total+1;
+            r++;
+        }
+        else if(st[i]=='s' && s==0)
+        {
+            total = total+1;
+            s++;
+        }
+        else if(st[i]=='t' && t==0)
+        {
+            total = total+1;
+            t++;
+        }
+        else if(st[i]=='u' && u==0)
+        {
+            total = total+1;
+            u++;
+        }
+        else if(st[i]=='v' && v==0)
+        {
+            total = total+1;
+            v++;
+        }
+        else if(st[i]=='w' && w==0)
+        {
+            total = total+1;
+            w++;
+        }
+        else if(st[i]=='x' && x==0)
+        {
+            total = total+1;
+            x++;
+        }
+        else if(st[i]=='y' && y==0)
+        {
+            total = total+1;
+            y++;
+        }
+        else if(st[i]=='z' && z==0)
+        {
+            total = total+1;
+            z++;
+        }
+
+    }
+    if(total%2==0)
+    {
+        cout << "CHAT WITH HER!\n";
+    }
+    else
+    {
+        cout << "IGNORE HIM!\n";
+    }
+    return 0;
+}
